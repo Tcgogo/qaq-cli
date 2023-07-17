134a746f221a820dc49a89538aa4860e5b546846
tanchian <tanchian@shencom.cn>
2023-07-17 17:45:02 +0800

    feat: 初始化脚手架, 兼容打包内置模块

--- packages/tcgogo-init/src/index.ts
+++ packages/tcgogo-init/src/index.ts
@@ -1,5 +1,11 @@
-export function init() {
-  console.log('init tcgogo212');
-}
+import { Command } from 'commander';
+import { version } from '../package.json';
 
-init();
+const program = new Command();
+
+program.on('--help', () => {
+  // console.log(`\r\n Run123 to understand the details\r\n `);
+});
+
+program.name('--version').description('CLI to some JavaScript string utilities').version(version);
+program.parse();
--- packages/tcgogo-init/vite.config.ts
+++ packages/tcgogo-init/vite.config.ts
@@ -3,17 +3,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    minify: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'index',
       // the proper extensions will be added
-      fileName: 'index',
-      formats: ['cjs', 'es', 'umd', 'iife'],
+      fileName: (format) => `index.js`,
+      formats: ['cjs'],
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: [],
+      external: ['fs', 'events', 'child_process', 'path', 'process'],
       output: {
         // // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         // globals: {
